4051faed451de943e84201d0c78982edbdf817a5
DanielChan03 <zhenguee03@gmail.com>
2025-02-12 19:06:16 +0800

    Update __init__.py , for courier splitted code

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -16,19 +16,35 @@ def create_app():
     from .admin import admin
     from .courier import courier
     from .AdminAuth import admin_auth 
-    from .CourierAuth import courier_auth
+    from webapp.Couriercode.CourierAuth import courier_auth
     from .ParcelManagerAuth import parcel_manager_auth
     from .parcel_manager import parcel_manager
+    from webapp.Couriercode.courierdashboard import courier_dashboard_bp
+    from webapp.Couriercode.courierprofile import courier_profile_bp
+    from webapp.Couriercode.report_parcel import report_parcel_bp
+    from webapp.Couriercode.notifications import notifications_bp
+    from webapp.Couriercode.collect_parcel import collect_parcel_bp
+    from webapp.Couriercode.view_manager_list import view_manager_list_bp
+    from webapp.Couriercode.manage_parcel_status import manage_parcel_status_bp
+    from webapp.Couriercode.view_reported_history import view_reported_history_bp
+
 
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
     app.register_blueprint(admin, url_prefix='/admin')
-    app.register_blueprint(courier, url_prefix='/courier')
     app.register_blueprint(admin_auth, url_prefix='/admin')
     app.register_blueprint(courier_auth, url_prefix='/courier')
     app.register_blueprint(parcel_manager, url_prefix='/parcel-manager')
     app.register_blueprint(parcel_manager_auth, url_prefix='/parcel-manager')
+    app.register_blueprint(courier_dashboard_bp,url_prefix='/courier')
+    app.register_blueprint(courier_profile_bp,url_prefix='/courier')
+    app.register_blueprint(report_parcel_bp,url_prefix='/courier')
+    app.register_blueprint(notifications_bp,url_prefix='/courier')
+    app.register_blueprint(collect_parcel_bp,url_prefix='/courier')
+    app.register_blueprint(view_manager_list_bp,url_prefix='/courier')
+    app.register_blueprint(manage_parcel_status_bp,url_prefix='/courier')
+    app.register_blueprint(view_reported_history_bp,url_prefix='/courier')
 
     # Import models and create tables
     from .models import StudentStaff, University, ParcelManager, SmartLocker, Courier, ParcelStatus, Parcel, Waitlist,Admin, Report  # Import all your models here
